85cfe9be5b8c651494f5496f796cc45eee3c3248
DymPro <126939126+DymPro@users.noreply.github.com>
2023-03-14 09:00:25 +0100

    Update settings.py

--- dynamic/settings.py
+++ dynamic/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ym%ll+jn8$t_!x6g4jyqiigwfs4n$ir6b8f9v2o*gim@xrd9-^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
