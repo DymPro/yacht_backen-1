5bb47b56c371b377e4a83d6dce6e5dd01fb5fbab
Satyajit Barik <satyajitbarik.dev@gmail.com>
2023-03-17 01:23:05 +0530

    updated

--- dynamic/settings.py
+++ dynamic/settings.py
@@ -53,31 +53,33 @@ CORS_ALLOWED_ORIGINS = [
     "http://www.ncpliso.com",
     "http://ncpliso.com",
     "http://35.154.109.204",
-    "https://35.154.109.204"
+    "https://35.154.109.204",
+    "localhost:3000",
+    "http://localhost:3000/"
 ]
 
 # CORS_ALLOW_ALL_ORIGINS: True
 
-CORS_ALLOW_METHODS = [
-    "DELETE",
-    "GET",
-    "OPTIONS",
-    "PATCH",
-    "POST",
-    "PUT",
-]
+# CORS_ALLOW_METHODS = [
+#     "DELETE",
+#     "GET",
+#     "OPTIONS",
+#     "PATCH",
+#     "POST",
+#     "PUT",
+# ]
 
-CORS_ALLOW_HEADERS = [
-    "accept",
-    "accept-encoding",
-    "authorization",
-    "content-type",
-    "dnt",
-    "origin",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
-]
+# CORS_ALLOW_HEADERS = [
+#     "accept",
+#     "accept-encoding",
+#     "authorization",
+#     "content-type",
+#     "dnt",
+#     "origin",
+#     "user-agent",
+#     "x-csrftoken",
+#     "x-requested-with",
+# ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
