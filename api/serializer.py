fee63d215d63d34972da17ae107141dcf1192863
parkson-tano <tanoparksonsilencer@gmail.com>
2023-03-23 08:44:10 +0100

    leave management

--- api/serializer.py
+++ api/serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import *
 from account.serializer import UserSerializer
 
+
 class PortDataSerializer(serializers.ModelSerializer):
     class Meta():
         model = PortData
@@ -22,8 +23,7 @@ class ImageSerializer(serializers.ModelSerializer):
     class Meta():
         model = Image
         fields = "__all__"
-        
-        
+         
 class DocumentSerializer(serializers.ModelSerializer):
     class Meta():
         model = Document
@@ -34,3 +34,31 @@ class PortDataV(serializers.ModelSerializer):
     class Meta:
         model = PortData
         fields = ['id', ]
+
+class LeaveSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Leave
+        fields = "__all__"
+
+class LeaveDataSerializer(serializers.ModelSerializer):
+    employee = UserSerializer(read_only=True)
+    approved_by = UserSerializer(read_only=True)
+    class Meta():
+        model = Leave
+        fields = "__all__"
+
+
+class LeavePolicySerializer(serializers.ModelSerializer):
+    class Meta():
+        model = LeavePolicy
+        fields = "__all__"
+
+class SpecialLeaveSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = SpecialLeave
+        fields = "__all__"
+
+class CompanyLeavePolicySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompanyLeavePolicy
+        fields = "__all__"
