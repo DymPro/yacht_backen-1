64914e6b0d1101ba635a6c2afff42635d6ebeef6
root <root@ip-172-26-9-214.ap-south-1.compute.internal>
2023-03-25 14:35:42 +0000

    from aws

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', LoginView.as_view()),
     path('register',
          RegisterView.as_view({'post': 'create'}), name='auth_register'),
+path('user/<pk>', RegisterView.as_view({'get': 'retrieve', 'patch': 'partial_update', 'delete': 'destroy', 'put': 'update' }), name='auth_re'),
 ]
