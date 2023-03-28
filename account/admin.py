35dd6225f03329620431b94e498f5794d647325d
root <root@ip-172-26-9-214.ap-south-1.compute.internal>
2023-03-28 14:05:10 +0000

    ff

--- account/admin.py
+++ account/admin.py
@@ -14,12 +14,14 @@ admin.site.site_title = 'NCPLISO'
 class CustomUserAdmin(UserAdmin):
     list_display = ['username','first_name','email','role']
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('role', 'device_token','gender','pincode','date_of_birth','is_hr','port', 'phone', 'position', 'address', 'nationality')}),
+        (None, {'fields': ('role', 'device_token','gender','pincode','date_of_birth','is_hr','port', 'phone', 'position','department', 'address', 'nationality')}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
         (None, {'fields': ('role',  'device_token',
-         'gender', 'pincode', 'date_of_birth', 'is_hr','port', 'phone', 'position', 'address', 'nationality')}),
+         'gender', 'pincode', 'date_of_birth', 'is_hr','port', 'phone', 'position','department', 'address', 'nationality')}),
     )
 
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Position)
+admin.site.register(Department)
