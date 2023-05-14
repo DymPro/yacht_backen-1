c00dde41cf07c7c82338189b90ac61843db9c3b2
Parkson Tano Daniel <tanoparksonsilencer@gmail.com>
2023-05-14 09:34:36 +0100

    Update models.py

--- api/models.py
+++ api/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from django.conf import settings
+from account.models import Department
 User = settings.AUTH_USER_MODEL
 # Create your models here.
 
