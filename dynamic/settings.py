52278c4987a4b2be9c7ea838f456e538b7c7200a
Parkson Tano Daniel <tanoparksonsilencer@gmail.com>
2023-06-20 06:20:12 +0100

    whie

--- dynamic/settings.py
+++ dynamic/settings.py
@@ -135,7 +135,7 @@ MEDIA_URL = '/media/'
 
 # CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
+CORS_ORIGIN_WHITELIST = [
     "https://www.ncpliso.com",
     "https://ncpliso.com",
     "http://www.ncpliso.com",
