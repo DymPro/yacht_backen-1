c33555bd68900a1b7ad2f55a3fad13f655450ff0
Parkson Tano Daniel <tanoparksonsilencer@gmail.com>
2023-06-26 10:26:17 +0100

    cors origin

--- dynamic/settings.py
+++ dynamic/settings.py
@@ -104,14 +104,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'https://ncpliso.com',
-    'https://www.ncpliso.com',
-    'http://ncpliso.com',
-    'http://www.ncpliso.com',
-
-]
 
 
